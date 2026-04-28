3aa175aacc0d081c43b0a4b46c7078cebad87496
sameer patil <sbpatil29@gmail.com>
2026-04-28 11:51:54 +0530

    Intents Example

---
+++ Intent/app/src/main/java/com/example/intent/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.intent
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var button: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        button=findViewById(R.id.button)
+        button.setOnClickListener {
+            var intent = Intent(this@MainActivity, activity_second::class.java)
+            intent.putExtra("message","message from first activity")
+            startActivity(intent)
+        }//button click listener
+    }
+}
